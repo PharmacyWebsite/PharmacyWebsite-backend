c36244efc0c95afa81b96dbe216c327d8845dad5
Miduna-11 <midusubramaniam@gmail.com>
2026-04-17 12:13:10 +0530

    AuthChanges

--- PharmacyOrderingSystem/PharmacyOrderingSystem/Program.cs
+++ PharmacyOrderingSystem/PharmacyOrderingSystem/Program.cs
@@ -1,104 +1,34 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using PharmacyOrderingSystem.Configurations;
-using PharmacyOrderingSystem.Data;
-using PharmacyOrderingSystem.Helpers;
-using PharmacyOrderingSystem.Middleware;
-using PharmacyOrderingSystem.Services;
-using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
-    ));
-
-
-builder.Services.Configure<JwtSettings>(
-    builder.Configuration.GetSection("JwtSettings"));
-
-
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddScoped<PasswordHasher>();
-builder.Services.AddScoped<JwtHelper>();
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+namespace PharmacyOrderingSystem
 {
-    
-    options.SwaggerDoc("v1", new OpenApiInfo
+    public class Program
     {
-        Title = "Pharmacy Ordering System API",
-        Version = "v1"
-    });
-
-   
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Enter JWT Token like: Bearer {your_token}",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+        public static void Main(string[] args)
         {
-            new OpenApiSecurityScheme
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
             {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
-    });
-});
-
-
-var key = Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]!);
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-        ValidAudience = builder.Configuration["JwtSettings:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
-});
-
-var app = builder.Build();
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    }
 }
-
-
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseMiddleware<RateLimitingMiddleware>();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
