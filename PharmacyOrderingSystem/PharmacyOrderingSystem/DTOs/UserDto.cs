07a640633221d4fc61a5e38b57dc65e727ad361f
Miduna-11 <midusubramaniam@gmail.com>
2026-04-17 14:19:09 +0530

    Usercontroller and user service

---
+++ PharmacyOrderingSystem/PharmacyOrderingSystem/DTOs/UserDto.cs
@@ -0,0 +1,22 @@
+namespace PharmacyOrderingSystem.DTOs
+{
+    
+    public class UserDto
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string Role { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; }
+    }
+
+    
+    public class UpdateUserDto
+    {
+        public string Name { get; set; } = "";
+    }
+}
